0f821ded9fd90fe447824eed97ac784a1aae1ab5
Juan Carlos <juanksoliveira@gmail.com>
2021-04-11 02:51:42 -0300

    feat(service): add UserService

---
+++ lib/services/user_service.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+
+import 'package:app_medical_monitor/models/user.dart';
+import 'package:app_medical_monitor/services/interfaces/service.dart';
+import 'package:app_medical_monitor/services/routes.dart';
+import 'package:http/http.dart' as http;
+
+class UserService implements Service<User> {
+  String _token;
+
+  UserService({required String token}) : this._token = token;
+
+  Future<List<User>> findAll({int page = 1, int size = 10}) async {
+    final String path = "/users?page=$page&size=$size";
+    final url = baseUrl.resolve(path);
+    final response = await http
+        .get(url, headers: headerWithAuth(_token))
+        .timeout(Duration(milliseconds: 1000));
+    if (response.statusCode == 200) {
+      final List<dynamic> bodyJson = jsonDecode(response.body);
+      final List<User> users = [];
+
+      bodyJson.forEach((element) {
+        final Map<String, dynamic> e = element;
+        final User user = User.fromJson(e);
+        users.add(user);
+      });
+      return users;
+    }
+
+    throw Exception("Unexpected error on UserService: findAll");
+  }
+
+  @override
+  Future<User> findByID(String id) async {
+    final String path = "/users/$id";
+    final url = baseUrl.resolve(path);
+    final response = await http
+        .get(url, headers: headerWithAuth(_token))
+        .timeout(Duration(milliseconds: 1000));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> body = jsonDecode(response.body);
+      final user = User.fromJson(body);
+      return user;
+    }
+
+    throw Exception("Unexpected error on UserService: findByID");
+  }
+
+  @override
+  Future<void> save(User user) async {
+    Uri url = baseUrl.resolve("/users");
+    Map<String, dynamic> bodyJson = user.toJson();
+    final body = jsonEncode(bodyJson);
+    final response = await http
+        .post(url, headers: headerWithAuth(_token), body: body)
+        .timeout(Duration(milliseconds: 1000));
+    if (response.statusCode == 201) {
+      return;
+    }
+
+    throw Exception("Unexpected error on UserService: save");
+  }
+
+  @override
+  Future<void> update(User data) async {
+    String id = data.id!;
+    Uri url = baseUrl.resolve("/users/$id");
+    Map<String, dynamic> bodyJson = data.toJson();
+    final body = jsonEncode(bodyJson);
+    final response = await http
+        .patch(url, headers: headerWithAuth(_token), body: body)
+        .timeout(Duration(milliseconds: 1000));
+    if (response.statusCode == 200) {
+      return;
+    }
+
+    throw Exception("Unexpected error on UserService: update");
+  }
+
+  @override
+  Future<void> remove(String id) async {
+    final String path = "/users/$id";
+    final Uri url = baseUrl.resolve(path);
+    final response = await http
+        .delete(url, headers: headerWithAuth(_token))
+        .timeout(Duration(milliseconds: 1000));
+    if (response.statusCode != 204) throw Exception("Remove failed");
+
+    throw Exception("Unexpected error on UserService: remove");
+  }
+}
