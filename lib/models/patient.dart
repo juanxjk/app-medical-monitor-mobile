a8a15e8e4c26e29a71b65aca3b245de4b4f471ab
Juan Carlos <juanksoliveira@gmail.com>
2021-04-12 23:27:42 -0300

    feat(models/Patient): add json serialization

--- lib/models/patient.dart
+++ lib/models/patient.dart
@@ -35,4 +35,73 @@ class Patient {
     this.comorbidities = const [],
     this.prognosis,
   });
+
+  Patient.fromJson(Map<String, dynamic> json)
+      : this.id = json['id'],
+        this.fullName = json['fullName'],
+        this.cpf = json['cpf'],
+        this.gender = getGenderFromString(json['gender']),
+        this.birthDate = DateTime.parse(json['birthDate']),
+        this.status = getStatusFromString(json['status']),
+        this.bed = json['bed'],
+        this.devices = json['devices'] ?? [],
+        this.illnesses = (json['illnesses'] as List<dynamic>)
+            .map((e) => e.toString())
+            .toList(),
+        this.comorbidities = (json['comorbidities'] as List<dynamic>)
+            .map((e) => e.toString())
+            .toList(),
+        this.prognosis = json['prognosis'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': this.id,
+      'fullName': this.fullName,
+      'cpf': this.cpf,
+      'gender': getGenderString(this.gender),
+      'birthDate': this.birthDate.toIso8601String(),
+      'status': getStatusString(this.status),
+      'bed': this.bed,
+      'devices': this.devices,
+      'illnesses': this.illnesses,
+      'comorbidities': this.comorbidities,
+      'prognosis': this.prognosis,
+    };
+  }
+
+  static getGenderFromString(String gender) {
+    return GenderType.values
+        .firstWhere((e) => e.toString() == 'GenderType.' + gender);
+  }
+
+  static getStatusFromString(String status) {
+    return PatientStatus.values
+        .firstWhere((e) => e.toString() == 'PatientStatus.' + status);
+  }
+
+  static String getGenderString(GenderType gender) {
+    switch (gender) {
+      case GenderType.masculine:
+        return 'masculine';
+      case GenderType.feminine:
+        return "feminine";
+      default:
+        throw Exception("Invalid gender");
+    }
+  }
+
+  static String getStatusString(PatientStatus status) {
+    switch (status) {
+      case PatientStatus.none:
+        return 'none';
+      case PatientStatus.waiting:
+        return 'waiting';
+      case PatientStatus.treatment:
+        return 'treatment';
+      case PatientStatus.discharged:
+        return 'discharged';
+      default:
+        throw Exception("Invalid status");
+    }
+  }
 }
