0633508088ad5a47267b8c41c68c0a1813335033
Juan Carlos <juanksoliveira@gmail.com>
2021-05-05 00:47:49 -0300

    feat(component/menu_add): add color and leading

--- lib/components/menu_add.dart
+++ lib/components/menu_add.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 class MenuAdd extends StatefulWidget {
   final String _title;
   final List<String> _list;
-  MenuAdd({required String title, required List<String> list})
+  final Color _color;
+  final Widget? _leading;
+
+  MenuAdd(
+      {required String title,
+      required List<String> list,
+      Color color = Colors.blue,
+      Widget? leading})
       : this._title = title,
-        this._list = list;
+        this._list = list,
+        this._color = color,
+        this._leading = leading;
   @override
   _MenuAddState createState() => _MenuAddState();
 }
@@ -26,8 +35,9 @@ class _MenuAddState extends State<MenuAdd> {
         return ListTile(
           title: Text(illness),
           leading: Icon(
-            Icons.circle,
-            size: 20,
+            Icons.label,
+            size: 25,
+            color: widget._color,
           ),
           trailing: IconButton(
             icon: Icon(Icons.delete),
@@ -53,10 +63,18 @@ class _MenuAddState extends State<MenuAdd> {
         });
     }
 
+    final leading = widget._leading != null
+        ? Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+            child: widget._leading,
+          )
+        : SizedBox.shrink();
+
     return Column(
       children: [
         Row(
           children: [
+            leading,
             Flexible(
               child: TextFormField(
                 controller: inputController,
